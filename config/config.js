ef6b90346f3fb09fd5ecd3ac447faf54a4e69b47
Nivesh Singh <nivesh2@gmail.com>
2016-04-22 09:08:43 +0530

    Cofig files created

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+          consumer_key:         'yourKey'
+          , consumer_secret:      'yourSecret'
+          , app_only_auth:        true
+          //, timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+};
