2c2bd864473aa606e0a01e90fe4af9e3100abfb4
Nivesh Singh <nivesh2@gmail.com>
2016-04-22 09:11:21 +0530

    app logic parted added

---
+++ app/route/base.route.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const route = function(app){
+    const twitter = rootRequire('app/controller/twitter.controller');
+
+    app.get('/',(req,res)=>{
+        res.redirect('/index.html');
+    });
+
+    app.post('/getBestTime',twitter.getBestTime);   
+
+};
+
+module.exports = route;
