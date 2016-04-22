e247715c49643b840810245de6d027ce9f8cdd65
Nivesh Singh <nivesh2@gmail.com>
2016-04-22 09:40:49 +0530

    index.html added

--- config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-          consumer_key:         'yourKey'
-          , consumer_secret:      'yourSecret'
-          , app_only_auth:        true
-          //, timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-};
